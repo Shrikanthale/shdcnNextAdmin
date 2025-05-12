cf39eaa9f93c9f9ae7ec90fcdd06aaa640ad80fd
shrikanthale <skanthale9@gmail.com>
2025-05-12 16:13:50 +0530

    integration api (trpc + prisma)

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,53 @@
 "use client"
 import Registerpage from "@/components/ui/registerpage";
 import { trpc } from "@/server/client";
+import { Trash2 } from "lucide-react";
+import { useState } from "react";
 export default function Home() {
+  
+    const [name, setName] = useState<string>("")
+    const [surname, setSurname] = useState<string>("")
   const getUsers = trpc.user.getUsers.useQuery()
+  const addUser = trpc.user.addUser.useMutation({
+    onSettled: () => {
+      getUsers.refetch()
+    }
+  })
+  const submitUser = () => {
+    addUser.mutate({ name, surname })
+    setName("")
+    setSurname("")
+  }
+  const deleteUser = trpc.user.deleteUser.useMutation({
+    onSettled: () => {
+      getUsers.refetch()
+    }
+  })
   return (
    <>
    {/* <Registerpage /> */}
-   <div className="flex min-h-screen flex-col items-center justify-between p-24">
+   <div className="flex min-h-screen flex-col items-center justify-between p-14">
      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
        {getUsers.data?.map((user) => (
          <div 
            key={user.name} 
-           className="p-6 rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition-shadow"
+           className="p-8 rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition-shadow"
          >
-           <div className="text-center space-y-2">
+           <div className="text-center flex flex-col justify-between items-center space-y-2 ">
              <h1 className="text-2xl font-bold capitalize">{user.name}</h1>
              <p className="text-xl text-gray-600 capitalize">{user.surname}</p>
+             <button onClick={() => deleteUser.mutate({ id: user.id })} className="bg-red-500 text-white p-2 rounded-md flex items-center gap-2">
+               <Trash2 className="" />
+             </button>
            </div>
          </div>
        ))}
+     </div>
+
+     <div className="flex flex-col items-center justify-center">
+      <input type="text" placeholder="Name" className="p-2 rounded-md border border-gray-300" value={name} onChange={(e) => setName(e.target.value)} /> <br />
+      <input type="text" placeholder="Surname" className="p-2 rounded-md border border-gray-300" value={surname} onChange={(e) => setSurname(e.target.value)} /> <br />
+      <button onClick={submitUser}>Add User</button>
      </div>
    </div>
    </>
--- server/routers/user.ts
+++ server/routers/user.ts
@@ -1,25 +1,45 @@
 import { procedure, router } from "../trpc";
 import { z } from "zod";
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
+
 export const userRouter = router({
-    getUsers : procedure.query(() => {
-        return [
-            {
-               name : "joe",
-              surname : "tribiani"
-            },
-            {
-                name : "monica chandler",
-                surname : "bing"
-            }
-        ]
+    getUsers : procedure.query(async () => {
+        // return [
+        //     {
+        //        name : "joe",
+        //       surname : "tribiani"
+        //     },
+        //     {
+        //         name : "monica chandler",
+        //         surname : "bing"
+        //     }
+        // ]
+        return await prisma.user.findMany()
     }),
 
     addUser : procedure.input(z.object({
         name : z.string(),
         surname : z.string()
-    })).mutation((opts)=>{
+    })).mutation(async (opts)=>{
         const {input} = opts
         //  call prisma
+        return await prisma.user.create({
+            data : {
+                name : input.name,
+                surname : input.surname
+            }
+        })
+    }),
+
+    deleteUser : procedure.input(z.object({
+        id : z.number()
+    })).mutation(async (opts)=>{
+        const {input} = opts
+        return await prisma.user.delete({
+            where : {id : input.id}
+        })
     })
 })
 
